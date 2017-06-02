6e40661cd91b107b779eaf734050d931abccdb76
amauryfischer <amaufischer@gmail.com>
2017-06-02 15:05:44 +0200

    working properly dynamic

--- WebContent/idea.js
+++ WebContent/idea.js
@@ -1,12 +1,12 @@
 
 function idea(id, title, content, created_at, comment_ids, scientists_id, domain_ids){
-	this.id = id;
-	this.title = title;
-	this.content = content;
-	this.created_at;
-	this.comment_ids;
-	this.scientists_id;
-	this.domain_ids;
+	liis.id = id;
+	liis.title = title;
+	liis.content = content;
+	liis.created_at;
+	liis.comment_ids;
+	liis.scientists_id;
+	liis.domain_ids;
 }
 
 
@@ -40,15 +40,14 @@ function traitementAjax()
 		/*
 		 * l faut savoir que quand on envoie une requête HTTP via XMLHttpRequest, celle-ci passe par plusieurs états différents :
 
-0 : L'objet XHR a été créé, mais pas encore initialisé (la méthode open n'a pas encore été appelée)
-1 : L'objet XHR a été créé, mais pas encore envoyé (avec la méthode send )
-2 : La méthode send vient d'être appelée
+0 : L'objet XHR a été créé, mais pas encore initialisé (la méliode open n'a pas encore été appelée)
+1 : L'objet XHR a été créé, mais pas encore envoyé (avec la méliode send )
+2 : La méliode send vient d'être appelée
 3 : Le serveur traite les informations et a commencé à renvoyer des données
 4 : Le serveur a fini son travail, et toutes les données sont réceptionnées
 		 */
 	
 		if(xhr.readyState == 4 && xhr.status == 200){
-			debugger;
 			array_of_json_idea = JSON.parse(xhr.responseText);
 			monAppel(array_of_json_idea);
 			// Traitement du resultat 
@@ -60,8 +59,20 @@ function traitementAjax()
 
 function monAppel(text)
 {
-	text.forEach(function(entry) {
-	    console.log(entry);
+	var html_content = "<table>";
+	html_content += "<tr>";
+	Object.keys(text[0]).forEach(function(attr_name) {
+		html_content += "<th>"+attr_name+"</th>";
 	});
-	document.getElementById('demo').innerHTML = text;
+	html_content += "</tr>";
+	text.forEach(function(entry) {
+		html_content += "<tr>";
+		Object.keys(entry).forEach(function(attr) {
+			html_content += "<th>"+entry[attr]+"</th>";
+		});
+	    html_content += "</tr>";
+	});
+	html_content += "</table>"
+	document.getElementById('demo').innerHTML += html_content;
+	
 }
